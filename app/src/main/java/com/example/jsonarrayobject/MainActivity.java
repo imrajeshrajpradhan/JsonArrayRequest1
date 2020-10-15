525b092ca071299e2d5976144429318b37738d25
imrajeshrajpradhan <rajeshpradhan745@gmail.com>
2020-10-15 11:23:42 +0530

    JsonArrayRequest

---
+++ app/src/main/java/com/example/jsonarrayobject/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.jsonarrayobject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+        mQueue = Volley.newRequestQueue(this);
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+    }
+    private void jsonParse() {
+        String url = "https://my-json-server.typicode.com/typicode/demo/db";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("posts");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject employee = jsonArray.getJSONObject(i);
+                                String title = employee.getString("title");
+                                int id = employee.getInt("id");
+                                mTextViewResult.append(title + ", " + String.valueOf(id) + "\n\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "SOmething went wrong", Toast.LENGTH_SHORT).show();
+                error.printStackTrace();
+            }
+        });
+        MySingleton.getInstance(MainActivity.this).addToRequestque(request);
+    }
+
+}
